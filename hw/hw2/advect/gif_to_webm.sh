d49b3c49b0bfdb2b0bed96582ca3fa58087fa1f4
Scott Fones <scott.fones@gmail.com>
2022-11-18 00:47:20 -0500

    removed banner output

--- hw/hw2/advect/gif_to_webm.sh
+++ hw/hw2/advect/gif_to_webm.sh
@@ -1,1 +1,1 @@
-ffmpeg -y -i advect_anim.gif -pix_fmt yuv420p -c:v libvpx-vp9 -crf 18 -b:v 0 advect_anim.webm
+ffmpeg -y -hide_banner -i advect_anim.gif -pix_fmt yuv420p -c:v libvpx-vp9 -crf 18 -b:v 0 advect_anim.webm
